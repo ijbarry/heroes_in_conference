09ced1a4bf84d3126e73b8f01c79d8755261c11e
Jonas <jonas.fiala.cardiff@gmail.com>
2019-02-07 16:56:12 +0100

    Add NUnit
    NUnit testing framework for testing C# code
    
    Add base client DB and example unit test
    
    The Database is unifinished, just so that we can have a unit test
    
    Update .gitignore
    
    Update .gitignore
    
    Ignore NUnit dir
    
    Add TextMesh to .gitignore
    
    Fix formatting
    
    Remove ignored files

---
+++ frontend/Assets/Scripts/Client/Database/DBAchievement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Contains all information about locally stored Achievement
+/// </summary>
+[Serializable]
+public class DBAchievement {
+    private long achievementID;
+    public long AchievementID { get => achievementID; }
+
+    private string achievementName;
+    public string AchievementName { get => achievementName; }
+
+    private string achievementDescription;
+    public string AchievementDescription { get => achievementDescription; }
+
+    private bool won;
+    public bool Won { get => won; set => won = value; }
+
+    public DBAchievement(long achievementID, string achievementName, string achievementDescription, bool wonAchievement) {
+        this.achievementID = achievementID;
+        this.achievementName = achievementName;
+        this.achievementDescription = achievementDescription;
+        this.won = wonAchievement;
+    }
+}
---
+++ frontend/Assets/Scripts/Client/Database/DBMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Contains all information about locally stored map graphics
+/// </summary>
+[Serializable]
+public class DBMap {
+    private long mapID;
+    public long MapID { get => mapID; }
+
+    private DateTime validBefore;
+    public DateTime ValidBefore { get => validBefore; set => validBefore = value; }
+
+    private string filePath;
+    public string FilePath { get => filePath; }
+
+    private string mapName;
+    public string MapName { get => mapName; }
+}
