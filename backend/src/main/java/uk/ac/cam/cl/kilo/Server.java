9d53c1c642703588faf167edf28e282409351d05
ijbar <ijb29@cam.ac.uk>
2019-02-10 23:12:43 +0000

    Login to Facebook with Spark

--- backend/src/main/java/uk/ac/cam/cl/kilo/Server.java
+++ backend/src/main/java/uk/ac/cam/cl/kilo/Server.java
@@ -1,83 +1,55 @@
 package uk.ac.cam.cl.kilo;
 
 import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.util.*;
+import static spark.Spark.*;
+package uk.ac.cam.cl.kilo.database;
 
 public class Server extends HttpServlet {
- 
-    @Override
-    //Handle GET request
-    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        if (request.getMethod().equals("POST")) {
-            doPost(request, response);
+  public static void main(String args[]){
 
-        } else if (request.getMethod().equals("GET")) {
-            doGet(request, response);
-
-        }
-        //otherwise consider redirect from FB
-        // put info from FB into DB
-        request.getRequestURL();
-        return;
-    }
-
-    @Override
-    //Handle GET request
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-       return;
-    }
-
-    // Handle POST request
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        // Set the response content type
-        response.setContentType("text; charset=UTF-8");
-        PrintWriter out = response.getWriter();
-
-        try {
-            String state = request.getParameter("state");
-            if(state == null || htmlFilter(state).isEmpty()){
+    get("/facebook/register/stage1", (request, response) -> {
+            String state = request.queryParamOrDefault("state","");
+            if(state == ""){
                 //didn't include valid state
                 return;
             }
 
             if (/*lookup state in database*/ false) {
                 //state not in db
-                //add state to db
-                response.sendRedirect("https://www.facebook.com/v3.2/dialog/oauth?&" +
+                //user not registered
+                response.redirect("https://www.facebook.com/v3.2/dialog/oauth?&" +
                         "client_id={2332527273433130}&" +
-                        "redirect_uri={"+/*redirect uri*/ "}&"  +
+                        "redirect_uri={"+/*redirect uri/facebook/register/stage2*/ "}&"  +
                         "state={"+state+"}");
             }
             else{
-                response.sendError(1,"Shouldn't be posting");
+                String code = //lookup in DB
+            if(state == "" || code == ""){
+                //error on FBs end
+                return;
+            }
+            else{
+              //need a way to send GET request to FB
+              //get back access token
+
+            }
+            }
+    });
+
+    get("/facebook/register/stage2", (request, response) -> {
+            String state = request.queryParamOrDefault("state","");
+            String code = request.queryParamOrDefault("code","");
+            if(state == "" || code == ""){
+                //error on FBs end
+                return;
+            }
+            else{
+              //need a way to send GET request to FB
+              //get back access token
+
             }
 
         }
-        finally {
-            out.close();
-        }
-    }
-
-    // Filter the string
-    private static String htmlFilter(String message) {
-        if (message == null) return null;
-        int len = message.length();
-        StringBuffer result = new StringBuffer(len + 20);
-        char aChar;
-
-        for (int i = 0; i < len; ++i) {
-            aChar = message.charAt(i);
-            switch (aChar) {
-                case '<': result.append("&lt;"); break;
-                case '>': result.append("&gt;"); break;
-                case '&': result.append("&amp;"); break;
-                case '"': result.append("&quot;"); break;
-                default: result.append(aChar);
-            }
-        }
-        return (result.toString());
-    }
+    });
+  }
 }
